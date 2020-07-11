0264f66f467486562224f7303f3fa2cf85c89c60
boboo <ma_yanboo@163.com>
2020-07-11 15:40:19 +0800

    数组：217. 存在重复元素

---
+++ Array/Solution217.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+/**
+ *
+ * 217. 存在重复元素
+ *
+ * 给定一个整数数组，判断是否存在重复元素。
+ *
+ * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3,1]
+ * 输出: true
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author myb
+ * @date 2020/7/10 22:17
+ * @description Solution217
+ */
+public class Solution217 {
+    public static void main(String[] args) {
+        int[] nums = {3, 1};
+        System.out.println(new Solution217().containsDuplicate(nums));
+    }
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return false;
+    }
+}
