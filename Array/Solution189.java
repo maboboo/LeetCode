bacb9ac2c5d131b565b821d9439c9482b6349133
boboo <ma_yanboo@163.com>
2020-07-20 21:54:14 +0800

    数组：189. 旋转数组

---
+++ Array/Solution189.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+/**
+ * 189. 旋转数组
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3,4,5,6,7] 和 k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author myb
+ * @date 2020/7/20 21:49
+ * @description Solution
+ */
+public class Solution189 {
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,4,5,6,7};
+        new Solution189().rotate(nums, 3);
+        Arrays.stream(nums).forEach(System.out::println);
+    }
+    public void rotate(int[] nums, int k) {
+        // k 有可能大于数组长度
+        k %= nums.length;
+        // 反转整个数组
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
